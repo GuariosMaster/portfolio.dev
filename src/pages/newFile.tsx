3040e1b03b3894cb3377d8702194ed145025f4a4
GuariosMaster <guariosdeveloper@gmail.com>
2024-02-02 18:38:32 -0500

    Tow commit the portfolio proyect

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,11 @@
+import Layout from '../layouts/Layout.astro';
+
+<Fragment>
+<Layout title="Welcome to Astro.">
+<main>
+<h1>Hey, soy Manuel <Badge>Disponible para Trabajar</Badge> </h1>
+</main>
+</Layout>
+
+
+</Fragment>;
